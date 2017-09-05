4c3235fc48c955eefc7ebeec28216c53c407e303
Mats Henrikson <mats@zakalwe.com>
2017-09-05 12:19:30 +1000

    Adding in the Splitter Tool.

--- lib/Views/UserInterface.jsx
+++ lib/Views/UserInterface.jsx
@@ -5,8 +5,9 @@ import version from '../../version';
 import StandardUserInterface from 'terriajs/lib/ReactViews/StandardUserInterface/StandardUserInterface.jsx';
 import MenuItem from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/MenuItem';
 import RelatedMaps from './RelatedMaps';
-import { Menu, ExperimentalMenu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
+import { Menu, Nav, ExperimentalMenu } from 'terriajs/lib/ReactViews/StandardUserInterface/customizable/Groups';
 import SplitPoint from 'terriajs/lib/ReactViews/SplitPoint';
+import ToggleSplitterTool from 'terriajs/lib/ReactViews/Map/Navigation/ToggleSplitterTool';
 
 import './global.scss';
 
@@ -28,6 +29,9 @@ export default function UserInterface(props) {
                 <RelatedMaps viewState={props.viewState} />
                 <MenuItem caption="About" href="about.html" key="about-link"/>
             </Menu>
+            <Nav>
+                <ToggleSplitterTool terria={props.viewState.terria} key="toggle-splitter-tool"/>
+            </Nav>
             <ExperimentalMenu>
                 <If condition={isBrowserSupportedAV()}>
                     <SplitPoint loadComponent={loadAugmentedVirtuality} viewState={props.viewState} terria={props.viewState.terria} experimentalWarning={true}/>
